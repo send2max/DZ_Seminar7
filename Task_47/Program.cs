ea08e7ad20b08a37fa71c64730361f01e4f3e787
Max <send2max@bk.ru>
2022-09-01 20:28:40 +0300

    Решена третья  задача

--- Task_47/Program.cs
+++ Task_47/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] GenerationMatrix(int row, int col, int min, int max)
+{
+    double[,] matrix = new double[row, col];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.NextDouble() * (max - min) + min;
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{Math.Round(matrix[i, j], 2),6} ");
+
+        }
+        Console.WriteLine("]");
+    }
+}
+
+double[,] array2d = GenerationMatrix(3, 4, -11, 11);
+PrintMatrix(array2d);
--- Task_52/Program.cs
+++ Task_52/Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+System.Console.WriteLine();
+Console.Clear();
+
+int[,] CreateMtrixRndInt(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) System.Console.Write($"{matrix[i, j],4},");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void AverageArithmetic(int[,] array)
+{
+     Console.WriteLine($"Среднее арифметическое элементов столбца массива");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        double meanarithmetic = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            meanarithmetic = meanarithmetic + array[j, i];
+        }
+        meanarithmetic = Math.Round(meanarithmetic / array.GetLength(1), 1);
+
+        Console.WriteLine($" {meanarithmetic}");
+    }
+
+
+}
+
+int[,] array = CreateMtrixRndInt(4, 4, 1, 10);
+PrintMatrix(array);
+Console.WriteLine ();
+AverageArithmetic(array);
